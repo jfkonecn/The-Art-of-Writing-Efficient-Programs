dceeb5feb7bd05202c77ab85391e95989383ff8f
John Konecny <24961694+jfkonecn@users.noreply.github.com>
2023-06-12 09:35:31 -0400

    a

---
+++ Chapter02/build.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+mkdir -p build
+clang++-11 -g -O3 -mavx2 -Wall -pedantic 01_substring_sort_a.C 01_substring_sort.C -lprofiler -o build/01_substring_sort
+CPUPROFILE=build/prof.data build/01_substring_sort
