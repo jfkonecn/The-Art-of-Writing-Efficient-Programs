a7ec8ed59530cd3270a1a51b3b46a785dfc4b9e5
John Konecny <24961694+jfkonecn@users.noreply.github.com>
2023-06-12 10:09:33 -0400

    a

--- Chapter02/build.sh
+++ Chapter02/build.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
 mkdir -p build
-clang++-11 -g -O3 -mavx2 -Wall -pedantic 01_substring_sort_a.C 01_substring_sort.C -lprofiler -o build/01_substring_sort
-CPUPROFILE=build/prof.data build/01_substring_sort
-google-pprof --text build/01_substring_sort build/prof.data > build/01_substring_sort.txt
+#clang++-11 -g -O3 -mavx2 -Wall -pedantic 01_substring_sort_a.C 01_substring_sort.C -lprofiler -o build/01_substring_sort
+#CPUPROFILE=build/prof.data build/01_substring_sort
+#google-pprof --text build/01_substring_sort build/01_substring_sort_prof.data
+
+#clang++-11 -g -O3 -mavx2 -Wall -pedantic 03_substring_sort_a.C 03_substring_sort.C -lprofiler -o build/03_substring_sort
+#CPUPROFILE=build/prof.data build/03_substring_sort
+#google-pprof --text build/03_substring_sort build/03_substring_sort_prof.data
+
+clang++-11 -g -O3 -mavx2 -Wall -pedantic 04_substring_sort_a.C 04_substring_sort.C -lprofiler -o build/04_substring_sort
+CPUPROFILE=build/prof.data build/04_substring_sort
+google-pprof --text build/04_substring_sort build/prof.data
